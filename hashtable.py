b8a67099e278a00df5c170215b1df512f51a54c5
AlaaSall <salsabilamobile636@gmail.com>
2025-12-07 10:17:49 +0700

    Create hashtable.py

---
+++ hashtable.py
@@ -0,0 +1,30 @@
+class HashTableChaining:
+    def __init__(self, size=10):
+        self.size = size
+        self.table = [[] for _ in range(size)]
+
+    def _hash(self, key):
+        return sum(ord(c) for c in str(key)) % self.size
+
+    def put(self, key, value):
+        index = self._hash(key)
+        for item in self.table[index]:
+            if item[0] == key:
+                item[1] = value
+                return
+        self.table[index].append([key, value])
+
+    def get(self, key):
+        index = self._hash(key)
+        for item in self.table[index]:
+            if item[0] == key:
+                return item[1]
+        return None
+
+    def remove(self, key):
+        index = self._hash(key)
+        for i, item in enumerate(self.table[index]):
+            if item[0] == key:
+                del self.table[index][i]
+                return True
+        return False
